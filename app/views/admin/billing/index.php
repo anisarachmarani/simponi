e5856999e98046d502de40bc0a1ea7fbf348e0c1
Indra Oktaviano <indra.oktaviano@gmail.com>
2022-09-06 19:54:57 +0700

    update

--- app/views/admin/billing/index.php
+++ app/views/admin/billing/index.php
@@ -144,33 +144,6 @@
                                         </div>
                                     </div>
 
-                                    <script src="<?php echo base_url() ?>assets/libs/datatables.net/js/jquery.dataTables.min.js"></script>
-    <script src="<?php echo base_url() ?>assets/libs/datatables.net-bs4/js/dataTables.bootstrap4.min.js"></script>
-    <!-- Buttons examples -->
-    <script src="<?php echo base_url() ?>assets/libs/datatables.net-buttons/js/dataTables.buttons.min.js"></script>
-    <script src="<?php echo base_url() ?>assets/libs/datatables.net-buttons-bs4/js/buttons.bootstrap4.min.js"></script>
-    <script src="<?php echo base_url() ?>assets/libs/jszip/jszip.min.js"></script>
-    <script src="<?php echo base_url() ?>assets/libs/pdfmake/build/pdfmake.min.js"></script>
-    <script src="<?php echo base_url() ?>assets/libs/pdfmake/build/vfs_fonts.js"></script>
-    <script src="<?php echo base_url() ?>assets/libs/datatables.net-buttons/js/buttons.html5.min.js"></script>
-    <script src="<?php echo base_url() ?>assets/libs/datatables.net-buttons/js/buttons.print.min.js"></script>
-    <script src="<?php echo base_url() ?>assets/libs/datatables.net-buttons/js/buttons.colVis.min.js"></script>
-    <!-- Responsive examples -->
-    <script src="<?php echo base_url() ?>assets/libs/datatables.net-responsive/js/dataTables.responsive.min.js"></script>
-    <script src="<?php echo base_url() ?>assets/libs/datatables.net-responsive-bs4/js/responsive.bootstrap4.min.js"></script>
-    <!-- Datatable init js -->
-    <script src="<?php echo base_url() ?>assets/js/pages/datatables.init.js"></script>
-    <!-- choices js -->
-    <script src="<?php echo base_url() ?>assets/libs/choices.js/public/assets/scripts/choices.min.js"></script>
-    <!-- color picker js -->
-    <script src="<?php echo base_url() ?>assets/libs/@simonwep/pickr/pickr.min.js"></script>
-    <script src="<?php echo base_url() ?>assets/libs/@simonwep/pickr/pickr.es5.min.js"></script>
-    <!-- datepicker js -->
-    <script src="<?php echo base_url() ?>assets/libs/flatpickr/flatpickr.min.js"></script>
-    <!-- init js -->
-    <script src="<?php echo base_url() ?>assets/js/pages/form-advanced.init.js"></script>
-
-
                                     <table id="datatable" class="table table-bordered dt-responsive nowrap w-100">
                                         <thead>
                                             <tr>
@@ -234,6 +207,31 @@
 
     <?php $this->load->view("admin/partials/script.php") ?>
     <!-- Required datatable js -->
+    <script src="<?php echo base_url() ?>assets/libs/datatables.net/js/jquery.dataTables.min.js"></script>
+    <script src="<?php echo base_url() ?>assets/libs/datatables.net-bs4/js/dataTables.bootstrap4.min.js"></script>
+    <!-- Buttons examples -->
+    <script src="<?php echo base_url() ?>assets/libs/datatables.net-buttons/js/dataTables.buttons.min.js"></script>
+    <script src="<?php echo base_url() ?>assets/libs/datatables.net-buttons-bs4/js/buttons.bootstrap4.min.js"></script>
+    <script src="<?php echo base_url() ?>assets/libs/jszip/jszip.min.js"></script>
+    <script src="<?php echo base_url() ?>assets/libs/pdfmake/build/pdfmake.min.js"></script>
+    <script src="<?php echo base_url() ?>assets/libs/pdfmake/build/vfs_fonts.js"></script>
+    <script src="<?php echo base_url() ?>assets/libs/datatables.net-buttons/js/buttons.html5.min.js"></script>
+    <script src="<?php echo base_url() ?>assets/libs/datatables.net-buttons/js/buttons.print.min.js"></script>
+    <script src="<?php echo base_url() ?>assets/libs/datatables.net-buttons/js/buttons.colVis.min.js"></script>
+    <!-- Responsive examples -->
+    <script src="<?php echo base_url() ?>assets/libs/datatables.net-responsive/js/dataTables.responsive.min.js"></script>
+    <script src="<?php echo base_url() ?>assets/libs/datatables.net-responsive-bs4/js/responsive.bootstrap4.min.js"></script>
+    <!-- Datatable init js -->
+    <script src="<?php echo base_url() ?>assets/js/pages/datatables.init.js"></script>
+    <!-- choices js -->
+    <script src="<?php echo base_url() ?>assets/libs/choices.js/public/assets/scripts/choices.min.js"></script>
+    <!-- color picker js -->
+    <script src="<?php echo base_url() ?>assets/libs/@simonwep/pickr/pickr.min.js"></script>
+    <script src="<?php echo base_url() ?>assets/libs/@simonwep/pickr/pickr.es5.min.js"></script>
+    <!-- datepicker js -->
+    <script src="<?php echo base_url() ?>assets/libs/flatpickr/flatpickr.min.js"></script>
+    <!-- init js -->
+    <script src="<?php echo base_url() ?>assets/js/pages/form-advanced.init.js"></script>
 
 </body>
 
