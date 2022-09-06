65444067b0f7a615de9f5e10fdb8d32f65826a64
Indra Oktaviano <indra.oktaviano@gmail.com>
2022-09-06 22:06:20 +0700

    update config

---
+++ app/config/database copy.php
@@ -0,0 +1,20 @@
+<?php  if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+$active_group = 'simponi';
+$active_record = TRUE;
+$db['simponi']['hostname'] = "localhost";
+$db['simponi']['username'] = "root";
+$db['simponi']['password'] = "";
+$db['simponi']['database'] = "simponi";
+$db['simponi']['dbdriver'] = "mysqli";
+$db['simponi']['dbprefix'] = '';
+$db['simponi']['pconnect'] = FALSE;
+$db['simponi']['db_debug'] = FALSE;
+$db['simponi']['cache_on'] = FALSE;
+$db['simponi']['cachedir'] = '';
+$db['simponi']['char_set'] = 'utf8';
+$db['simponi']['dbcollat'] = 'utf8_general_ci';
+$db['simponi']['swap_pre'] = '';
+$db['simponi']['autoinit'] = TRUE;
+$db['simponi']['stricton'] = FALSE;
+/* End of file database.php */
+/* Location: ./application/config/database.php */
